c88e0e8e405b6ce7e640a4c32ac057310ab8462d
toverux <mtouverey@alembic-dev.com>
2015-09-25 22:36:07 +0200

    add fortune command

---
+++ src/AppBundle/Command/FortuneCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FortuneCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('gk:fortune')
+            ->setDescription('Ragots, citations et voyance.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->write(`fortune|cowsay`);
+    }
+}
